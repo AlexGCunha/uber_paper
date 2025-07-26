b89fb7af0d1aa74242afdb02522ec1be0b9469b8
Alexandre Cunha <alexandregomesdacunha@gmail.com>
2025-07-26 16:28:29 -0300

    Revert "working, results pre rogerio talk"
    This reverts commit 090d23563012b30036eafe0af0e30d9c03168293.

--- ub_drafts.R
+++ ub_drafts.R
@@ -1,47 +1,13 @@
 
-library(devtools)
-library(cowplot)
-library(tidyverse)
-library(arrow)
-library(did)
-library(data.table)
-library(fixest)
-library(modelsummary)
-library(HonestDiD)
-library(BMisc)
+rais_path = "C:/Users/xande/OneDrive/Documentos/Doutorado/Research/RAIS"
+rais_nova = read_parquet(paste0(rais_path, "/rais_2010_teste.parquet"))
+cnova = colnames(rais_nova)
+rais_antiga = read_parquet(paste0(rais_path, "/rais_2010.parquet"))
+cantiga = colnames(rais_antiga)
+
+rais_antiga = rais_antiga %>% 
+  select(-c(tamanho_estabelecimento, tipo_estabelecimento))
 
 
-
-#abrir dataset
-df = read_parquet("../data/ub_rais_merged.parquet")
-
-#parametros:
-#dropar primeiros municipios
-dropar_primeiros_munics = 0
-minimo_cidades = 10
-maximo_periodo = 20202
-# minimo_habs = 50000
-
-#definir controles
-controles = as.formula(" ~ lmean_income+ unem_rate_m + inf_rate_m + lpop +lpop2")
-sem_controles = as.formula("~ 1")
-
-######################################
-#Ajustes
-######################################
-#Dropar primeiros municípios e ultimos municipios
-if(dropar_primeiros_munics == 1){
-  df = df[!(tem_uber == 1 & semestre_entrada %in% c(20141, 20142, 20151, 20152))]
-}
-
-#Dropar minimo de habitantes
-# df = df[pop14 >= minimo_habs]
-
-#dropar grupos onde o numero de cidades tratadas no periodo é menor que o minimo
-df[, conta_cidade_grupo := length(unique(id_municipio)), by = .(semestre_entrada_did)]
-df = df[conta_cidade_grupo >=minimo_cidades]
-
-
-
-source("../uber2/ub_funcoes_auxiliares.R")
-dia = "202504"
+write_parquet(rais_nova, "../data/testenova.parquet")
+write_parquet(rais_antiga, "../data/testeantiga.parquet")
--- ub_regressions.R
+++ ub_regressions.R
@@ -104,7 +104,7 @@ p2 %>% print()
 #save original overall att to use later
 or_att_emp = m2[[2]]$overall.att
 
-ggsave("../Output/202504/emprego_noeduc.png", height = 5, width = 9)
+ggsave("../Output/202504/emprego_noeduc.png", height = 4.5, width = 8)
 
 ######################################
 # Emprego privado- Por Educação
@@ -125,7 +125,7 @@ plot_grid(p1,p2, nrow = 1)
 #save original overall att for lths to use later
 or_att_emp_lths = m1[[2]]$overall.att
 
-ggsave("../Output/202504/emprego_privado_escolaridade.png", height = 5, width = 9)
+ggsave("../Output/202504/emprego_privado_escolaridade.png", height = 4.5, width = 8)
 
 ######################################
 # Emprego temporário e Meio Período
@@ -162,7 +162,7 @@ p1 = plot_es(m1, title = 'Less Than High School')
 p2 = plot_es(m2, title = "High School or More")
 plot_grid(p1,p2, nrow = 1)
 
-ggsave("../Output/202504/emprego_temporario_educ.png", height = 5, width = 9)
+ggsave("../Output/202504/emprego_temporario_educ.png", height = 4.5, width = 8)
 
 ######################################
 # Emprego Meio Período - Por educação
@@ -181,7 +181,7 @@ ggsave("../Output/202504/emprego_temporario_educ.png", height = 5, width = 9)
   plot_grid(p1,p2, nrow = 1)
 
 
-ggsave("../Output/202504/emprego_meio_periodo_educ.png", height = 5, width = 9)
+ggsave("../Output/202504/emprego_meio_periodo_educ.png", height = 4.5, width = 8)
 
 
 ######################################
@@ -217,7 +217,7 @@ plot_grid(p1,p2, nrow = 1)
 #save original overall att to use later
 or_att_wage_hs = m2[[2]]$overall.att
 
-ggsave("../Output/202504/wages_educ.png", height = 5, width = 9)
+ggsave("../Output/202504/wages_educ.png", height = 4.5, width = 8)
 
 
 ######################################
@@ -232,31 +232,31 @@ m2 = regressao_cs(variavel_dependente = "emprego_rural",
                   dep_em_log= 1, controles_use = controles, 
                   control_group = "notyettreated")
 
-m3 = regressao_cs(variavel_dependente = "salario_publico",
-                  dep_em_log= 1, controles_use = controles, 
-                  control_group = "notyettreated")
-
-m4 = regressao_cs(variavel_dependente = "salario_rural",
-                  dep_em_log= 1, controles_use = controles, 
-                  control_group = "notyettreated")
-
 
 p1 = plot_es(m1, title = 'Public Employment')
 p2 = plot_es(m2, title = "Rural Employment")
-p3 = plot_es(m3, title = "Public Wages")
-p4 = plot_es(m4, title = "Rural Wages")
-plot_grid(p1,p2, p3, p4, nrow = 2)
+plot_grid(p1,p2, nrow = 1)
 
-ggsave("../Output/202504/emprego_salario_publico.png", height = 9, width = 9)
+ggsave("../Output/202504/emprego_publico.png", height = 4.5, width = 8)
 
 ######################################
 # Salário Publico e Rural
 ######################################
 set.seed(456)
+m1 = regressao_cs(variavel_dependente = "salario_publico",
+                  dep_em_log= 1, controles_use = controles, 
+                  control_group = "notyettreated")
+
+m2 = regressao_cs(variavel_dependente = "salario_rural",
+                  dep_em_log= 1, controles_use = controles, 
+                  control_group = "notyettreated")
 
 
+p1 = plot_es(m1, title = 'Public Wages')
+p2 = plot_es(m2, title = "Rural Wages")
+plot_grid(p1,p2, nrow = 1)
 
-
+ggsave("../Output/202504/salario_publico.png", height = 4.5, width = 8)
 
 
 
@@ -305,7 +305,7 @@ title = ggdraw() +
              fontface='bold')
 plot_grid(title, pg, ncol=1, rel_heights=c(0.1, 1))
 
-ggsave("../Output/202504/emprego_privado_escolaridade_universal.png", height = 5, width = 9)
+ggsave("../Output/202504/emprego_privado_escolaridade_universal.png", height = 4.5, width = 8)
 
 
 ######################################
@@ -328,7 +328,7 @@ title = ggdraw() + draw_label("Log Wages",
                               fontface='bold')
 plot_grid(title, pg, ncol=1, rel_heights=c(0.1, 1))
 
-ggsave("../Output/202504/wages_educ_universal.png", height = 9, width = 9)
+ggsave("../Output/202504/wages_educ_universal.png", height = 4.5, width = 8)
 
 
 
@@ -549,17 +549,17 @@ for(i in c(1:1000)){
 #plot for overall employment
 ggplot(fake_att, aes(att_emp))+geom_histogram()+
   geom_vline(aes(xintercept = or_att_emp))+ theme_minimal()+
-  labs(x = 'Fake ATTs', y = 'Count')+aux_theme
+  labs(x = 'Fake ATTs', y = 'Count')+aux_theme()
 
 #plot for lths employment
 ggplot(fake_att, aes(att_emp_lths))+geom_histogram()+
   geom_vline(aes(xintercept = or_att_emp_lths))+ theme_minimal()+
-  labs(x = 'Fake ATTs', y = 'Count')+aux_theme
+  labs(x = 'Fake ATTs', y = 'Count')+aux_theme()
 
 #plot for hs wages
 ggplot(fake_att, aes(wage_hs))+geom_histogram()+
   geom_vline(aes(xintercept = or_att_wage_hs))+ theme_minimal()+
-  labs(x = 'Fake ATTs', y = 'Count')+aux_theme
+  labs(x = 'Fake ATTs', y = 'Count')+aux_theme()
 
 
 
